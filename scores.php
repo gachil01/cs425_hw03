da96d0c32c1059e1e3b65f37b015893e767c49c2
George Achilleos <gachil01@cs.ucy.ac.cy>
2018-11-20 22:49:41 +0200

    add scores.php

---
+++ scores.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Quiz Game</title>
+    <meta name="description" content="Score section.">
+    <meta name="keywords" content="Homework3, EPL425, Quiz">
+    <meta name="author" content="George Achilleos">
+    <link rel="shortcut icon" type="image/png" href="images\favicon.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="common.css">
+    <link rel="stylesheet" href="scores.css">
+</head>
+<body>
+<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="help.php">Help</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="scores.php">High Scores</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="image">
+  <div class="text">
+    <h1>Quiz Game</h1>
+  </div>
+</div>
+
+<div class = "score">
+  <p>The top scores are:</p>
+</div>
+
+<div class="footer">
+  <button id="myButton"><a href="#top" style="color: white">Top</a></button>
+</div>
+</body>
+</html>
